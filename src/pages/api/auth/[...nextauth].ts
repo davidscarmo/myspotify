87937f61d518cb45c3ae3a7450496bfcbd79fd10
David Carmo <davidsc2812@gmail.com>
2021-11-24 04:46:55 -0300

    Criacao do redirect no callback e redirecionamento caso o user já esteja logado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,19 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-var scope = 'user-read-private user-read-email';
+
+var scope = "user-read-private user-read-email";
 export default NextAuth({
   providers: [
     Providers.Spotify({
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      scope
+      scope,
     }),
   ],
   callbacks: {
     async signIn(user, account, profile) {
       try {
-        console.log("aoba");
-        console.log(user)
+        console.log(user);
         console.log(account);
         return true;
       } catch (err) {
@@ -21,11 +21,14 @@ export default NextAuth({
         return false;
       }
     },
+    async redirect(url) {
+      url = "http://localhost:3000/PlaylistPage";
+      return url;
+    },
     // session: async (session, profile) => {
     //   console.log("In session");
     //   console.log(session);
     //   console.log(profile);
-
 
     //   return Promise.resolve(session);
     // },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import { signIn } from "next-auth/client";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
+import { getSession, signIn } from "next-auth/client";
 
-export default function Home() {
+export default function Home({ session }) {
   return (
     <>
-      {" "}
-      <button onClick={() => signIn("spotify")}>
-        Logar com o spotify
-      </button>{" "}
+      {!session && (
+        <button onClick={() => signIn("spotify")}>Logar com o spotify</button>
+      )}
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  if (session) {
+    return {
+      redirect: {
+        destination: "/PlaylistPage",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
