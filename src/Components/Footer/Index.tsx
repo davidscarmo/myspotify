1b60ec1ceb4a15967b26a350f1f645ea4f4450b7
David Carmo <davidsc2812@gmail.com>
2021-11-24 18:24:41 -0300

    criacao do component footer

---
+++ src/Components/Footer/Index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>mySpotify - 2021</p>
+    </footer>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import { Provider as NextAuthProvider } from "next-auth/client";
 import { Header } from "../Components/Header";
+import { Footer } from "../Components/Footer/Index";
+
 function MyApp({ Component, pageProps }) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </NextAuthProvider>
   );
 }
